31c3e3cfd6c20a9b073575bd6a27d734d276cac8
dox404 <muktarul.hoque@hotmail.com>
2022-03-10 16:06:16 +0530

    commit

---
+++ c-practice/pattern13.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+int i,j,k;
+for(i=1;i<=4;i++){
+    k=i;
+    for(j=0;j<=7;j++){
+        if(j>=5-i&&j<=3+i){
+            printf("%d",k);
+            if(k<=4){
+                k++;
+            }
+            else{
+                k--;
+            }
+        }
+        else{
+            printf(" ");
+
+        }
+        
+    }
+    printf("\n");
+}
+return 0;
+}
